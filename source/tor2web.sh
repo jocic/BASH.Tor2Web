5337e88e95e907430b1a60e6a5f8ffcc9a5a9511
Đorđe Jocić <office@djordjejocic.com>
2019-08-26 18:17:09 -0400

    Added blank script.

---
+++ source/tor2web.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+###################################################################
+# Script Author: Djordje Jocic                                    #
+# Script Year: 2019                                               #
+# Script License: MIT License (MIT)                               #
+# =============================================================== #
+# Personal Website: http://www.djordjejocic.com/                  #
+# =============================================================== #
+# Permission is hereby granted, free of charge, to any person     #
+# obtaining a copy of this software and associated documentation  #
+# files (the "Software"), to deal in the Software without         #
+# restriction, including without limitation the rights to use,    #
+# copy, modify, merge, publish, distribute, sublicense, and/or    #
+# sell copies of the Software, and to permit persons to whom the  #
+# Software is furnished to do so, subject to the following        #
+# conditions.                                                     #
+# --------------------------------------------------------------- #
+# The above copyright notice and this permission notice shall be  #
+# included in all copies or substantial portions of the Software. #
+# --------------------------------------------------------------- #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND        #
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT     #
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,    #
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, RISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR   #
+# OTHER DEALINGS IN THE SOFTWARE.                                 #
+###################################################################
+
+##################################
+# Step 1 - Export Core Variables #
+##################################
+
+export J_T2W_USER_ID="$(id -u)";
+export J_T2W_SOURCE_DIR="$(cd -- "$(dirname -- "$0")" && pwd -P)";
+export J_T2W_VERSION="1.0.0";
+export J_T2W_CONF_DIR="tor2web";
+export J_T2W_CONF_FILE="basic.conf";
+export J_T2W_SUPPRESS_WARNING="no";
+
+##############################
+# Step 2 - Print Hello World #
+##############################
+
+echo "Hello World!"
