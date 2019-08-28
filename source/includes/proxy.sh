9a3f048ec1fd495985d91615ef85e06b70013178
Đorđe Jocić <office@djordjejocic.com>
2019-08-28 10:28:01 -0400

    Altered setup procedure.

--- source/includes/proxy.sh
+++ source/includes/proxy.sh
@@ -56,12 +56,24 @@
 
 setup()
 {
-    # Step 1 - Check If Setup
+    # Core Variables
     
+    setup_status="$(get_config_param 'setup_status')";
     
-    # Step 2 - Setup Proxy
+    # Logic
     
-    prepare_system && setup_tor && setup_apache;
+    if [ -z "$setup_status" ]; then
+        
+        prepare_system && setup_tor && \
+            setup_tor2web && setup_apache;
+        
+    else
+        
+        printf "[X] Tor2Web has already been setup on this machine.\n";
+        
+    fi
+    
+    return 0;
 }
 
 ###################
@@ -89,9 +101,10 @@ prepare_system()
     
     printf "[*] Preparing the system...\n";
     
-    yum clean all && yum update -y && \
-        yum install python2-pip python-devel libffi-devel -y && \
-            yum groupinstall "Development Tools" -y;
+    yum clean all && yum update -y && yum install centos-release-scl -y;
+    
+    yum install python36-pip python36-devel libffi-devel -y && \
+        yum groupinstall "Development Tools" -y;
     
     return 0;
 }
@@ -113,6 +126,26 @@ setup_tor()
     
     yum install tor privoxy -y && \
         systemctl start tor && systemctl enable tor;
+}
+
+# Setups <i>Tor2Web</i> on the machine.
+# 
+# @author: Djordje Jocic <office@djordjejocic.com>
+# @copyright: 2019 MIT License (MIT)
+# @version: 1.0.0
+# 
+# @return integer
+#   It always returns <i>0</i> - SUCCESS.
+
+
+setup_tor2web()
+{
+    # Logic
+    
+    printf "[*] Setting up Tor2Web...\n";
+    
+    wget https://deb.globaleaks.org/install-tor2web.sh && \
+        chmod +x install-tor2web.sh && ./install-tor2web.sh;
 }
 
 # Setups <i>Apache2</i> on the machine.
